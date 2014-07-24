a1e6618f0b7134687291a80acd79e83a5fed6c34
Calvin Metcalf <cmetcalf@appgeo.com>
2014-07-24 11:39:07 -0400

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+var promise = require('lie');
+var qs = require('querystring');
+function ajax(url, method, body){
+  body = body || {};
+  return promise(function(resolve,reject){
+    var data = qs.stringify(body);
+    if (method === 'get') {
+      url = url + '?' + data;
+    }
+    var ajax = new XMLHttpRequest();
+    ajax.open(method, url, true);
+    function onLoad(){
+      if(ajax.status>399){
+        reject(new Error(ajax.status));
+      } else {
+        resolve(ajax.response);
+      }
+      ajax.removeEventListener("load", onLoad, false);
+    }
+    ajax.addEventListener("load", onLoad, false);
+    if (method === 'get') {
+      ajax.send();
+    } else {
+      xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+      ajax.send(data);
+    }
+  });
+}
+exports.get = function (url, data) {
+  return ajax(url, 'get', data);
+};
+exports.post = function (url, data) {
+  return ajax(url, 'post', data);
+};
---
+++ readme.md
@@ -0,0 +1,10 @@
+ajax
+====
+
+`npm install calvinmetcalf/ajax`
+
+```js
+var ajax = require('ajax-lie');
+ajax.get(url, data);//promise
+ajax.post(url, data);//promise
+```
